cad316625792e1d1049a431bad6f1da0438f8b42
Walter Weber <walter.weber@rwth-aachen.de>
2016-09-09 02:20:06 +0200

    added a basic package filter

--- nicerfuturesite.user.js
+++ nicerfuturesite.user.js
@@ -7,51 +7,202 @@
 // @match        https://pucatrade.com/trades/packages
 
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @grant        none
+// @grant   GM_getValue
+// @grant   GM_setValue
+// @grant   GM_listValues
+// @grant   GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
+    var minPoints = 0;
+    var minCards = 2;
+    var maxCards = "max";
+    
+    if(GM_listValues().indexOf("minPoints") > -1){
+        minPoints = GM_getValue("minPoints");
+    }
+    
+    if(GM_listValues().indexOf("minCards") > -1){
+        minCards = GM_getValue("minCards");
+    }
+    
+    if(GM_listValues().indexOf("maxCards") > -1){
+        maxCards = GM_getValue("maxCards");
+    }
+    
+
     waitForKeyElements ("#trades .item", actionFunction);
 
     function actionFunction (jNode) {
-    //-- DO WHAT YOU WANT TO THE TARGETED ELEMENTS HERE.
         var elements = document.getElementsByClassName("item");
-        for (var j = 0, len = elements.length; j < len; j++) {
+        //var currentChild = elements[0];
+        var currentChild = jNode.context;
+        var nextChild = null;
+
         var content = null;
-        for (var i = 0; i < elements[j].childNodes.length; i++) {
-            if (elements[j].childNodes[i].className == "content") {
-                content = elements[j].childNodes[i];
-                break;
-            }
-        }
-        var cards = null;
-        for (var i = 0; i < content.childNodes.length; i++) {
-            if (content.childNodes[i].className == "card") {
-                cards = content.childNodes[i];
-                break;
-            }
-        }
-        
-        
+
+        content = getChildByName(currentChild, "content");
+
+        var cards = getChildByName(content, "card");
+
+
         content.appendChild(cards);
+        //console.log(content);
+        //console.log(countCards(cards));
+
+        filterPackage(content, minPoints, minCards, maxCards);
         if(cards.children.length > 5){
             console.log("moren than 5 children");
-        } 
+        }        
 
     }
-       // jNode.css ("background", "yellow"); // example
+    
+    waitForKeyElements ("#trades .filter.filter-menu", addFilterDiv);
+    function addFilterDiv(jNode){
+        console.log(jNode);
+        
+        var apply_filter_div = document.createElement("DIV");
+        apply_filter_div.className += " filter-item search p-filter";
+        
+        var minPoint_d = document.createElement("div");
+        var minCard_d = document.createElement("div");
+        var maxCard_d = document.createElement("div");
+        var apply_filter_btn = document.createElement("BUTTON");
+        var minPoint_f = document.createElement("input");
+        var minCard_f = document.createElement("input");
+        var maxCard_f = document.createElement("input");
+        var minPoint_l = document.createElement("div");
+        var minCard_l = document.createElement("div");
+        var maxCard_l = document.createElement("div");
+        
+        apply_filter_btn.className += " btn filterbtn short deepshadow";
+        
+        minPoint_l.innerHTML = "Min Points";
+        minPoint_l.className += " label current";
+        minPoint_d.className += " ";
+        minPoint_f.className += " filter-input";
+        minPoint_f.value = minPoints;
+        minPoint_d.appendChild(minPoint_l);
+        minPoint_d.appendChild(minPoint_f);
+        
+        minCard_l.innerHTML = "Min Cards";
+        minCard_l.className += " label current";
+        minCard_d.className += " ";
+        minCard_f.className += " filter-input";
+        minCard_f.value = minCards;
+        minCard_d.appendChild(minCard_l);
+        minCard_d.appendChild(minCard_f);
+        /*
+        maxCard_l.innerHTML = "Max Cards";
+        maxCard_l.className += " label current";
+        maxCard_d.className += " ";
+        maxCard_f.className += " filter-input";
+        maxCard_f.value = maxCards;
+        maxCard_d.appendChild(maxCard_l);
+        maxCard_d.appendChild(maxCard_f);
+        */
+        var t = document.createTextNode("Apply");
+        apply_filter_btn.appendChild(t);
+        
+        
+         apply_filter_div.appendChild(minPoint_d);
+        apply_filter_div.appendChild(minCard_d);
+         apply_filter_div.appendChild(maxCard_d);
+        apply_filter_div.appendChild(apply_filter_btn);
+       
+        
+        jNode.context.appendChild(apply_filter_div);              
+
+        apply_filter_btn.onclick =function() {
+            GM_setValue("minPoints", minPoint_f.value);
+            GM_setValue("minCards", minCard_f.value);
+            GM_setValue("maxCards", maxCard_f.value);
+            location.reload();
+        };
+
+    }
+
+    //content has to be a single node of class "content" from the "item" class div on package view
+    function filterPackage(content, minPoints, minCards, maxCards){
+
+        //console.log("minPoints: " + minPoints + " minCards: " + minCards + " maxCards: " + maxCards + "\r\n");
+        //console.log(content.childNodes[0].textContent);
+        var cards = getChildByName(content, "card");
+        var count = countCards(cards);
+        
+        if(maxCards == "max"){
+            if((parseInt(content.childNodes[0].textContent) < minPoints) || (count<minCards)){
+                
+                content.parentNode.style.display="none";
+            }
+        }else{
+            if((parseInt(content.childNodes[0].textContent) < minPoints) || (count < minCards) || (count>maxCards)){
+                content.parentNode.style.display="none";
+            }
+        }
+
+    }
+
+    //has to be passed a "card" class node collection
+    function countCards(cards){
+        var cnt  = 0;
+        var cur_c = cards.getElementsByClassName("row")[0];
+
+        while(cur_c !== null){
+            cnt = cnt + parseInt(cur_c.getElementsByClassName("number")[0].textContent);
+            cur_c = cur_c.nextSibling;
+        }
+        return cnt;
+    }
+
+    function filterPackages(minPoints, minCards, maxCards){
+        var packages = document.getElementsByClassName("item");
+        
+        //console.log(packages);
+
+        var currentChild = packages.item(0);
+        //console.log(currentChild);
+        var nextChild = null;
+        var content = null;
+
+        while(currentChild !== null){
+
+            nextChild = currentChild.nextSibling;
+            content = getChildByName(currentChild, "content");
+            //console.log(content);
+            //console.log(currentChild);
+            //console.log(minPoints);
+            //console.log(content.childNodes[0].textContent);
+            if(content.childNodes[0].textContent <= minPoints){
+                content.parentNode.style.display="none";
+            }
+            currentChild = nextChild;
+        }
+        
+        
     }
     
+    function getChildByName(node, name){
+        var child = null;
+        for (var i = 0; i < node.childNodes.length; i++) {
+            if (node.childNodes[i].className == name) {
+                child = node.childNodes[i];
+                break;
+            }
+         }
+        return child;
+    }
+
     waitForKeyElements ("body > undefined:nth-child(18) > div > span > div > div > div > div.tabs.tab-area > div.tab-content > div > div.items > div > span > div:nth-child(1)", actionFunction2);
-    
+
     function actionFunction2 (jNode) {
-       console.log("Now im running action function 2");
+       //console.log("Now im running action function 2");
         var elements = document.getElementsByClassName("card-details");
-        console.log("So many cards: ");
-        console.log(elements.length);
+        //console.log("So many cards: ");
+        //console.log(elements.length);
         var d = document.createElement("div");
-        d.innerHTML = "<span class=\"text letter\" >Cards: "+elements.length+"</span>"
+        d.innerHTML = "<span class=\"text letter\" >Cards: "+elements.length+"</span>";
         var totalcards = d.firstChild;
         
         var cost = document.getElementsByClassName("cost");
