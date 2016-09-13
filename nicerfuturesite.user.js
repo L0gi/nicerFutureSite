507640f16f53fbdc1e8f88d3db933d6e5aa804da
Walter Weber <walter.weber@rwth-aachen.de>
2016-09-14 01:05:35 +0200

    cleanup + filter
    cleaned up js
    filter now supports max cards

--- nicerfuturesite.user.js
+++ nicerfuturesite.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://pucatrade.com/trades/packages
+// @match        https://pucatrade.com/*
 
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant   GM_getValue
@@ -15,30 +15,34 @@
 
 (function() {
     'use strict';
-    var minPoints = 0;
+    
+        var minPoints = 0;
     var minCards = 2;
     var maxCards = "max";
-    
-    if(GM_listValues().indexOf("minPoints") > -1){
+
+    if (GM_listValues().indexOf("minPoints") > -1) {
         minPoints = GM_getValue("minPoints");
     }
-    
-    if(GM_listValues().indexOf("minCards") > -1){
+
+    if (GM_listValues().indexOf("minCards") > -1) {
         minCards = GM_getValue("minCards");
     }
-    
-    if(GM_listValues().indexOf("maxCards") > -1){
+
+    if (GM_listValues().indexOf("maxCards") > -1) {
         maxCards = GM_getValue("maxCards");
     }
-    
 
-    waitForKeyElements ("#trades .item", actionFunction);
+    if (window.location.href == "https://pucatrade.com/trades/packages") {
+        waitForKeyElements("#trades .item", actionFunction);
+        waitForKeyElements("#trades .filter.filter-menu", addFilterDiv);
+    }
 
-    function actionFunction (jNode) {
+
+
+    function actionFunction(jNode) {
         var elements = document.getElementsByClassName("item");
         //var currentChild = elements[0];
         var currentChild = jNode.context;
-        var nextChild = null;
 
         var content = null;
 
@@ -48,23 +52,23 @@
 
 
         content.appendChild(cards);
-        //console.log(content);
-        //console.log(countCards(cards));
 
-        filterPackage(content, minPoints, minCards, maxCards);
-        if(cards.children.length > 5){
-            console.log("moren than 5 children");
-        }        
+
+        if (filterPackage(content, minPoints, minCards, maxCards)) {
+            currentChild.parentElement.appendChild(currentChild);
+        }
+
 
     }
-    
-    waitForKeyElements ("#trades .filter.filter-menu", addFilterDiv);
-    function addFilterDiv(jNode){
-        console.log(jNode);
-        
+
+
+
+
+    function addFilterDiv(jNode) {
+
         var apply_filter_div = document.createElement("DIV");
         apply_filter_div.className += " filter-item search p-filter";
-        
+
         var minPoint_d = document.createElement("div");
         var minCard_d = document.createElement("div");
         var maxCard_d = document.createElement("div");
@@ -75,9 +79,9 @@
         var minPoint_l = document.createElement("div");
         var minCard_l = document.createElement("div");
         var maxCard_l = document.createElement("div");
-        
+
         apply_filter_btn.className += " btn filterbtn short deepshadow";
-        
+
         minPoint_l.innerHTML = "Min Points";
         minPoint_l.className += " label current";
         minPoint_d.className += " ";
@@ -85,7 +89,7 @@
         minPoint_f.value = minPoints;
         minPoint_d.appendChild(minPoint_l);
         minPoint_d.appendChild(minPoint_f);
-        
+
         minCard_l.innerHTML = "Min Cards";
         minCard_l.className += " label current";
         minCard_d.className += " ";
@@ -93,7 +97,7 @@
         minCard_f.value = minCards;
         minCard_d.appendChild(minCard_l);
         minCard_d.appendChild(minCard_f);
-        /*
+
         maxCard_l.innerHTML = "Max Cards";
         maxCard_l.className += " label current";
         maxCard_d.className += " ";
@@ -101,64 +105,67 @@
         maxCard_f.value = maxCards;
         maxCard_d.appendChild(maxCard_l);
         maxCard_d.appendChild(maxCard_f);
-        */
+
         var t = document.createTextNode("Apply");
         apply_filter_btn.appendChild(t);
-        
-        
-         apply_filter_div.appendChild(minPoint_d);
-        apply_filter_div.appendChild(minCard_d);
-         apply_filter_div.appendChild(maxCard_d);
-        apply_filter_div.appendChild(apply_filter_btn);
-       
-        
-        jNode.context.appendChild(apply_filter_div);              
 
-        apply_filter_btn.onclick =function() {
+
+        apply_filter_div.appendChild(minPoint_d);
+        apply_filter_div.appendChild(minCard_d);
+        apply_filter_div.appendChild(maxCard_d);
+        apply_filter_div.appendChild(apply_filter_btn);
+
+
+        jNode.context.appendChild(apply_filter_div);
+
+        apply_filter_btn.onclick = function() {
             GM_setValue("minPoints", minPoint_f.value);
             GM_setValue("minCards", minCard_f.value);
-            GM_setValue("maxCards", "max");
+            GM_setValue("maxCards", maxCard_f.value); //until max cards are supported
             location.reload();
         };
 
     }
 
     //content has to be a single node of class "content" from the "item" class div on package view
-    function filterPackage(content, minPoints, minCards, maxCards){
+    function filterPackage(content, minPoints, minCards, maxCards) {
 
-        //console.log("minPoints: " + minPoints + " minCards: " + minCards + " maxCards: " + maxCards + "\r\n");
-        //console.log(content.childNodes[0].textContent);
         var cards = getChildByName(content, "card");
         var count = countCards(cards);
-        
-        if(maxCards == "max"){
-            if((parseInt(content.childNodes[0].textContent) < minPoints) || (count<minCards)){
-                
-                content.parentNode.style.display="none";
+
+        if (maxCards == "max") {
+            if ((parseInt(content.childNodes[0].textContent) < minPoints) || (count < minCards)) {
+                content.parentNode.style.display = "none";
+                return 1;
+            } {
+                return 0;
             }
-        }else{
-            if((parseInt(content.childNodes[0].textContent) < minPoints) || (count < minCards) || (count>maxCards)){
-                content.parentNode.style.display="none";
+        } else {
+            if ((parseInt(content.childNodes[0].textContent) < minPoints) || (count < minCards) || (count > maxCards)) {
+                content.parentNode.style.display = "none";
+                return 1;
+            } else {
+                return 0;
             }
         }
 
     }
 
     //has to be passed a "card" class node collection
-    function countCards(cards){
-        var cnt  = 0;
+    function countCards(cards) {
+        var cnt = 0;
         var cur_c = cards.getElementsByClassName("row")[0];
 
-        while(cur_c !== null){
+        while (cur_c !== null) {
             cnt = cnt + parseInt(cur_c.getElementsByClassName("number")[0].textContent);
             cur_c = cur_c.nextSibling;
         }
         return cnt;
     }
 
-    function filterPackages(minPoints, minCards, maxCards){
+    function filterPackages(minPoints, minCards, maxCards) {
         var packages = document.getElementsByClassName("item");
-        
+
         //console.log(packages);
 
         var currentChild = packages.item(0);
@@ -166,7 +173,7 @@
         var nextChild = null;
         var content = null;
 
-        while(currentChild !== null){
+        while (currentChild !== null) {
 
             nextChild = currentChild.nextSibling;
             content = getChildByName(currentChild, "content");
@@ -174,41 +181,41 @@
             //console.log(currentChild);
             //console.log(minPoints);
             //console.log(content.childNodes[0].textContent);
-            if(content.childNodes[0].textContent <= minPoints){
-                content.parentNode.style.display="none";
+            if (content.childNodes[0].textContent <= minPoints) {
+                content.parentNode.style.display = "none";
             }
             currentChild = nextChild;
         }
-        
-        
+
+
     }
-    
-    function getChildByName(node, name){
+
+    function getChildByName(node, name) {
         var child = null;
         for (var i = 0; i < node.childNodes.length; i++) {
             if (node.childNodes[i].className == name) {
                 child = node.childNodes[i];
                 break;
             }
-         }
+        }
         return child;
     }
 
-    waitForKeyElements ("body > undefined:nth-child(18) > div > span > div > div > div > div.tabs.tab-area > div.tab-content > div > div.items > div > span > div:nth-child(1)", actionFunction2);
+    waitForKeyElements("body > undefined:nth-child(18) > div > span > div > div > div > div.tabs.tab-area > div.tab-content > div > div.items > div > span > div:nth-child(1)", actionFunction2);
 
-    function actionFunction2 (jNode) {
-       //console.log("Now im running action function 2");
+    function actionFunction2(jNode) {
+        //console.log("Now im running action function 2");
         var elements = document.getElementsByClassName("card-details");
         //console.log("So many cards: ");
         //console.log(elements.length);
         var d = document.createElement("div");
-        d.innerHTML = "<span class=\"text letter\" >Cards: "+elements.length+"</span>";
+        d.innerHTML = "<span class=\"text letter\" >Cards: " + elements.length + "</span>";
         var totalcards = d.firstChild;
-        
+
         var cost = document.getElementsByClassName("cost");
         cost[0].insertBefore(totalcards, cost[0].firstChild);
 
-       // jNode.css ("background", "yellow"); // example
+        // jNode.css ("background", "yellow"); // example
     }
-    
+
 })();
